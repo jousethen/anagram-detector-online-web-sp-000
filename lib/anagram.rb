4e025704611378583aa30d8516398073766f0033
Jouse then <jthen93@gmail.com>
2021-02-24 21:49:17 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,10 +10,15 @@ class Anagram
   
   def match (anagram)
     anagram_list = []
+    sorted_word = @word.split(//).sort!.join
     
     anagram.each do |w|
-      sorted_w = w.split(//).sort{|a| a}.join
-      
+      sorted_w = w.split(//).sort.join
+      if sorted_w = sorted_word
+        anagram_list << w
+      end
     end
+    
+    return anagram_list
   end
 end
